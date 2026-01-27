8d3b58253d61abcf30b36a2b2b0349369f1e3011
christenally <13616443+Christenally@users.noreply.github.com>
2026-01-26 18:43:43 -0600

    Tailwind Project 1

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"], // This path ensures the extension scans all HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
